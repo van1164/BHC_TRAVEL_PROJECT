42e44f40c8503fc906b53b93ff92b89dd29f9643
KIM <vkn123@naver.com>
2023-02-03 10:12:47 +0900

    Start_Project

---
+++ BE/HYEN_SHAN/src/main/java/com/BHC/TRAVEL/HYEN_SHAN/HyenShanApplication.java
@@ -0,0 +1,13 @@
+package com.BHC.TRAVEL.HYEN_SHAN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyenShanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyenShanApplication.class, args);
+	}
+
+}
