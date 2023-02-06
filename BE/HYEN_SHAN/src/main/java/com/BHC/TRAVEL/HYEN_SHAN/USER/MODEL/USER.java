3f9b4e9dc0fddbaf9e1461631e7a46417ebd6704
van133 <vkn123@naver.com>
2023-02-06 20:50:29 +0900

    Update mongodb

--- BE/HYEN_SHAN/src/main/java/com/BHC/TRAVEL/HYEN_SHAN/USER/MODEL/USER.java
+++ BE/HYEN_SHAN/src/main/java/com/BHC/TRAVEL/HYEN_SHAN/USER/MODEL/USER.java
@@ -1,16 +1,27 @@
 package com.BHC.TRAVEL.HYEN_SHAN.USER.MODEL;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@Document(collection = "USER")
 public class USER {
+    @Id
     private int useridx;
     private String ID;
     private String password;
     private String nickname;
 
+    @Builder
+    public USER(String ID, String password, String nickname){
+        this.ID = ID;
+        this.password = password;
+        this.nickname = nickname;
+    }
 }
--- BE/HYEN_SHAN/src/main/java/com/BHC/TRAVEL/HYEN_SHAN/USER/UserController.java
+++ BE/HYEN_SHAN/src/main/java/com/BHC/TRAVEL/HYEN_SHAN/USER/UserController.java
@@ -1,26 +1,40 @@
 package com.BHC.TRAVEL.HYEN_SHAN.USER;
 
 import com.BHC.TRAVEL.HYEN_SHAN.USER.MODEL.USER;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
 @RequestMapping("/User")
 public class UserController {
+
+    @Autowired
+    MongoTemplate mongoTemplate;
+    ObjectMapper objectMapper = new ObjectMapper();
     @ResponseBody
     @GetMapping("test")
     public HashMap<String,HashMap> mapping_Test(){
         HashMap<String,HashMap> test = new HashMap<>();
         HashMap<String,String> UserProperty = new HashMap<>();
-        UserProperty.put("ID","TEST");
-        test.put("USER_property",UserProperty);
+        USER user = USER.builder().ID("van133").password("ttink1245!").nickname("샨").build();
+        mongoTemplate.insert(user);
+        UserProperty.put("sdf","sdf");
+        System.out.println("TEST");
+        test.put("Sdfsdf",UserProperty);
         return test;
     }
 
 }
+
+
